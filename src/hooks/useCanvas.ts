0c236048415ebd3e648d506f8dbc1fa3b73658af
Sp1r1tual <andriy0383@gmail.com>
2025-11-22 18:09:57 +0200

    fix: remove blur

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -96,8 +96,16 @@ const useCanvas = (
     if (!ctx) return;
 
     const { width, height } = getContainerSize();
-    canvas.width = width;
-    canvas.height = height;
+
+    const dpr = window.devicePixelRatio || 1;
+
+    canvas.width = width * dpr;
+    canvas.height = height * dpr;
+
+    canvas.style.width = `${width}px`;
+    canvas.style.height = `${height}px`;
+
+    ctx.scale(dpr, dpr);
 
     ctx.clearRect(0, 0, width, height);
 
